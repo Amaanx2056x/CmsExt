099fe4061be53f525cbcbc0e6c9f03491de50f54
Amaanx2056x <weirdghost123@gmail.com>
2021-01-25 20:46:19 +0530

    removed node modules

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
-//mongoose.connect('mongodb+srv://cms-admin:cms-admin@cluster0.ao5hu.mongodb.net/cms?retryWrites=true&w=majority',{useUnifiedTopology: true,useNewUrlParser: true, useFindAndModify: false, useCreateIndex:true})
-mongoose.connect('mongodb://localhost:27017/test000x', {
+mongoose.connect('mongodb+srv://cms-admin:cms-admin@cluster0.ao5hu.mongodb.net/cms?retryWrites=true&w=majority', {
   useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true
 })
+/*mongoose.connect('mongodb://localhost:27017/test000x', {
+  useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true
+})*/
 .then((db)=> {
   console.log('Connected!')
 }).catch((e)=> {
